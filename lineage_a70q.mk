f1e8c8b4f28cf7a3e0ed890b3096ec49a8f5688a
Ataberk Özen <ataberkozen123@gmail.com>
2022-01-28 01:36:29 +0300

    halium: call halium.mk instead

--- lineage_a70q.mk
+++ lineage_a70q.mk
@@ -6,14 +6,11 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from a70q device
 $(call inherit-product, device/samsung/a70q/device.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_a70q
